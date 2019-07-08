960a92e950da735a99971406753e1aac604bfc26
kesavarthini <kesi@publicisgroupe.net>
2019-07-08 16:26:18 +0530

    Added Data, Values

--- src/main/java/com/pack/lombokdemo/Emp.java
+++ src/main/java/com/pack/lombokdemo/Emp.java
@@ -1,14 +1,29 @@
 package com.pack.lombokdemo;
 
-import lombok.Getter;
+//import lombok.Data;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.EqualsAndHashCode;
+//import lombok.Getter;
+//import lombok.RequiredArgsConstructor;
+//import lombok.Setter;
+//import lombok.ToString;
+import lombok.Value;
 
-@Getter
+//@Getter
+//@Setter
+//@ToString
+//@RequiredArgsConstructor
+//@AllArgsConstructor
+//@EqualsAndHashCode
+
+//@Data
+@Value
 public class Emp {
 
 	private String name;
 	private int age;
 	private float salary;
-	private String designation;
-	
+	private String Designation;
 	
 }

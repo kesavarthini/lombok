541ca80707c6fdc8c01446493237e208df5424ce
kesavarthini <kesi@publicisgroupe.net>
2019-07-08 16:14:44 +0530

    lombok demo

---
+++ src/main/java/com/pack/lombokdemo/Emp.java
@@ -0,0 +1,14 @@
+package com.pack.lombokdemo;
+
+import lombok.Getter;
+
+@Getter
+public class Emp {
+
+	private String name;
+	private int age;
+	private float salary;
+	private String designation;
+	
+	
+}
---
+++ src/test/java/com/pack/lombokdemo/AppTest.java
@@ -0,0 +1,10 @@
+package com.pack.lombokdemo;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+}
